2f3e7dd4bff2c82ac185db1a6c1bc5d723d37dbd
Hugh Kennedy <hughskennedy@gmail.com>
2014-06-27 15:32:15 -0700

    handle falsey values

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function isRequire(word) {
   word = word || 'require'
 
   return function(node) {
-    var c = node.callee
+    var c = node && node.callee
     return c
       && c.name === word
       && c.type === 'Identifier'
